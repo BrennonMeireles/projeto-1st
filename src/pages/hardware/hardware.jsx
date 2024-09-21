fc42bb0929ca0d3b436a2c2b28608c03c7f000fe
ThiagoCruz00 <thiagohfs.cruz@gmail.com>
2024-09-21 16:47:16 -0300

    feat: Criação da dunção Hardware

---
+++ src/pages/hardware/hardware.jsx
@@ -0,0 +1,12 @@
+import Hardware from "../../components/header-hardware/hardware";
+
+
+export default function PageHardware(){
+    return(
+        <>
+            <div className="gif-hardware">
+                <Hardware />
+            </div>
+        </>
+    )
+}
