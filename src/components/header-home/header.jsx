03707bb561902e3407cfaa502bc774981835ab69
ThiagoCruz00 <thiagohfs.cruz@gmail.com>
2024-09-21 11:39:13 -0300

    feat: Estrutura da header-home

---
+++ src/components/header-home/header.jsx
@@ -0,0 +1,25 @@
+import './style-header.css'
+import LogoSenai from '../../assets/img/Logo SENAI.svg'
+
+export default function HomeHeader() {
+    return (
+        <>
+        <div className='gif'>
+            <header className='headerhome-container'>
+                <img src={LogoSenai} alt="" />
+                <div className="links">
+                    <a href="">Matérias</a>
+                    <a href="">Especifícações</a>
+                    <a href="">Créditos</a>
+                </div>
+            </header>
+            <main className='text-header'>
+                <h1>DESENVOLVIMENTO DE SISTEMAS</h1>
+                <h4 className='Curso'>C U R S O</h4>
+                <h4 className='Senai'>SENAI</h4>
+            </main>
+        </div>
+
+        </>
+    )
+}
