d290442dae4872d7506b9afe0a7191513273ea5b
BrennonMeireles <brennonmeireles7@gmail.com>
2024-10-05 11:07:28 -0300

    feat: created title component

---
+++ src/components/title/title.jsx
@@ -0,0 +1,8 @@
+export default function Title({title, subTitle}){
+    return(
+        <div className="text-header">
+            <h1>{title}</h1>
+            <h4 className='Senai'>{subTitle}</h4>
+        </div>
+    )
+}
